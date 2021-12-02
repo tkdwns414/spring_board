aa79ad52223b91c666ca7696ba3007788d2939fb
tkdwns414 <sangjune2000@naver.com>
2021-12-02 19:59:51 +0900

    Create MemoryMemberRepository

---
+++ my-board/src/main/java/practice/myboard/domain/member/MemoryMemberRepository.java
@@ -0,0 +1,45 @@
+package practice.myboard.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(),member);
+
+        return member;
+    }
+
+    @Override
+    public Member findByID(Long id) {
+        return store.get(id);
+    }
+
+    @Override
+    public Optional<Member> findByLoginId(String loginId) {
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        store.remove(id);
+    }
+}
