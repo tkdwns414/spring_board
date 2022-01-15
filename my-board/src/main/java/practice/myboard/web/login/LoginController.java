d7b62dad3dfcd6697463b43d53b5dd04df85169c
tkdwns414 <sangjune2000@naver.com>
2022-01-15 23:04:01 +0900

    Create LoginController

---
+++ my-board/src/main/java/practice/myboard/web/login/LoginController.java
@@ -0,0 +1,21 @@
+package practice.myboard.web.login;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import practice.myboard.domain.login.LoginService;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute LoginForm form){
+        return 'login/loginForm';
+    }
+}
