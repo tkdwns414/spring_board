f33a741909437cb29f1f4caf5b1e9042dd38b35a
tkdwns414 <sangjune2000@naver.com>
2022-01-16 01:27:31 +0900

    Feat: Add login, logout

--- my-board/src/main/java/practice/myboard/web/login/LoginController.java
+++ my-board/src/main/java/practice/myboard/web/login/LoginController.java
@@ -3,9 +3,17 @@ package practice.myboard.web.login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import practice.myboard.domain.login.LoginService;
+import practice.myboard.domain.member.Member;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -18,4 +26,39 @@ public class LoginController {
     public String loginForm(@ModelAttribute LoginForm form){
         return "login/loginForm";
     }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                        @RequestParam(defaultValue = "/") String redirectURL,
+                        HttpServletRequest request){
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute("loginMember", loginMember);
+
+        return "redirect:" + redirectURL;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/posts";
+    }
+
 }
